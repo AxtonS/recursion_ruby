900083a6bd095c111bf420367e08b75de5753d70
Axton Smith <axtonsmith@gmail.com>
2022-07-22 17:44:33 -0400

    Finish recursive fibonacci array

--- lib/recursion.rb
+++ lib/recursion.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require 'pry-byebug'
+
 def fibs(num)
   arr = []
   x = 0
@@ -9,5 +11,15 @@ def fibs(num)
     arr[x] = arr[x - 2] + arr[x - 1] if x >= 2
     x += 1
   end
+  arr
+end
+
+def fibs_rec(num)
+  return [] if num.zero?
+  return [0] if num == 1
+  return [0, 1] if num == 2
+  binding.pry
+  arr = fibs_rec(num - 1)
+  arr << arr[-2] + arr[-1]
   arr
 end
