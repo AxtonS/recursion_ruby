f791b96235ad1a7a1ab0c7cf000f679f023f4748
Axton Smith <axtonsmith@gmail.com>
2022-07-22 15:41:50 -0400

    Finish fibs method, question 1

--- lib/recursion.rb
+++ lib/recursion.rb
@@ -1,3 +1,13 @@
 # frozen_string_literal: true
 
-def fibs; end
+def fibs(num)
+  arr = []
+  x = 0
+  while x < num
+    arr[0] = 0 if x == 0
+    arr[1] = 1 if x == 1
+    arr[x] = arr[x - 2] + arr[x - 1] if x >= 2
+    x += 1
+  end
+  arr
+end
