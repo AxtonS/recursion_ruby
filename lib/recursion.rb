54b35b199404e4a2b7820aa8b50ac349bb24683f
Axton Smith <axtonsmith@gmail.com>
2022-07-22 20:55:09 -0400

    Finish merge_sort method

--- lib/recursion.rb
+++ lib/recursion.rb
@@ -1,12 +1,10 @@
 # frozen_string_literal: true
 
-require 'pry-byebug'
-
 def fibs(num)
   arr = []
   x = 0
   while x < num
-    arr[0] = 0 if x == 0
+    arr[0] = 0 if x.zero?
     arr[1] = 1 if x == 1
     arr[x] = arr[x - 2] + arr[x - 1] if x >= 2
     x += 1
@@ -18,8 +16,36 @@ def fibs_rec(num)
   return [] if num.zero?
   return [0] if num == 1
   return [0, 1] if num == 2
-  binding.pry
+
   arr = fibs_rec(num - 1)
   arr << arr[-2] + arr[-1]
   arr
+end
+
+def merge_sort(arr)
+  return arr if arr.length <= 1
+
+  arr_a = merge_sort(arr[0..arr.length / 2 - 1])
+  arr_b = merge_sort(arr[arr.length / 2..])
+  i_a = 0
+  i_b = 0
+  i_c = 0
+  arr_c = []
+  while i_a < arr_a.length || i_b < arr_b.length
+    if arr_a[i_a] == nil
+      arr_c[i_c] = arr_b[i_b]
+      i_b += 1
+    elsif arr_b[i_b] == nil
+      arr_c[i_c] = arr_a[i_a]
+      i_a += 1
+    elsif (arr_a[i_a] < arr_b[i_b])
+      arr_c[i_c] = arr_a[i_a]
+      i_a += 1
+    else
+      arr_c[i_c] = arr_b[i_b]
+      i_b += 1
+    end
+    i_c += 1
+  end
+  arr_c
 end
